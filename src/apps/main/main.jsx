c1161972e731a61a2f8bb950e08e2c28b8ea57cc
Lario dos Santos Diniz <lariodiniz@gmail.com>
2021-01-31 16:55:17 -0300

    update routes to hashrouter

--- src/apps/main/main.jsx
+++ src/apps/main/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import '../../common/dependencies'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import Home from '../../views/home/home'
 import Rules from '../../views/rules/rules'
 
 const main = props => (
-    <BrowserRouter>
+    <HashRouter >
         <Switch>
-            <Route path="/oficial5c" exact > <Home /></Route>
-            <Route path="/oficial5c/regras" > <Rules /></Route>
+            <Route path="/" exact > <Home /></Route>
+            <Route path="/regras" > <Rules /></Route>
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 )
 
 export default main
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,16 +11,16 @@ const navbar = (props) => {
         
         return (
             <Navbar expand="lg" variant="dark" className="navbar-color">
-                <Navbar.Brand href="/oficial5c"><img
+                <Link to="/" className="nav-brand"><img
         src={icone}
         height="40"
         alt="React Bootstrap logo"
-      /></Navbar.Brand>
+      /></Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                        <Link to="/oficial5c" className="nav-link">Home</Link>
-                        <Link to="/oficial5c/regras" className="nav-link">Regras</Link>
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/regras" className="nav-link">Regras</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
